f33de1cb8f486dc8628b55bf068a442f29e207e0
HTL2910 <hathanhlong2910@gmail.com>
2024-04-14 11:50:29 +0700

    add trap feature

---
+++ Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public float speed = 5f; // Tốc độ di chuyển
+    public float xDirection = 1f; // Hướng theo trục x
+    public float yDirection = 0f; // Hướng theo trục y
+    public float zDirection = 0f; // Hướng theo trục z
+
+    void Update()
+    {
+        // Vector chỉ hướng di chuyển
+        Vector3 direction = new Vector3(xDirection, yDirection, zDirection);
+
+        // Di chuyển đối tượng
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TrapRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapRotation : MonoBehaviour
+{
+    public float swingSpeed = 2f; // Tốc độ quay
+    public float swingAngle = 45f; // Góc quay tối đa
+    private float initialAngle; // Góc ban đầu
+
+    void Start()
+    {
+        initialAngle = transform.rotation.eulerAngles.z; // Góc ban đầu của rìu
+    }
+
+    void Update()
+    {
+        // Tính toán góc quay dựa trên hàm sin
+        float angle = initialAngle + Mathf.Sin(Time.time * swingSpeed) * swingAngle;
+
+        // Quay rìu
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
