92a861ceb3e85355106f152778c29d25de4f99de
HTL2910 <hathanhlong2910@gmail.com>
2024-04-16 14:13:08 +0700

    complete UI

--- Assets/KTinteractiveProp/Volum 01/Scripts/LookMainCamera.cs
+++ Assets/KTinteractiveProp/Volum 01/Scripts/LookMainCamera.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +6,32 @@ namespace KTintercativeProp
 {
     public class LookMainCamera : MonoBehaviour
     {
-        // Update is called once per frame
+
         void Update()
         {
-            transform.LookAt(Camera.main.transform, Vector3.down);
+            // Lấy vị trí của camera
+            Vector3 cameraPosition = Camera.main.transform.position;
+
+            // Tạo vector hướng từ đối tượng đến camera
+            Vector3 lookDirection = cameraPosition - transform.position;
+
+            // Chuyển đổi vector hướng thành hướng quay
+            Quaternion targetRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+
+            // Gán hướng quay cho đối tượng
+            transform.rotation = targetRotation;
+            Quaternion currentRotation = transform.rotation;
+
+            // Tạo một quaternion mới đại diện cho quay 180 độ quanh trục y
+            Quaternion rotationToAdd = Quaternion.Euler(0f, 180f, 0f);
+
+            // Kết hợp quay mới với quay hiện tại của đối tượng
+            Quaternion newRotation = currentRotation * rotationToAdd;
+
+            // Gán hướng quay mới cho đối tượng
+            transform.rotation = newRotation;
         }
+
+
     }
 }
