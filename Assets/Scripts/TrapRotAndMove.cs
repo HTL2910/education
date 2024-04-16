32572d24ec46ea46fac768c81edf568d341ea498
HTL2910 <hathanhlong2910@gmail.com>
2024-04-16 10:05:04 +0700

    complete trap

---
+++ Assets/Scripts/TrapRotAndMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapRotAndMove : MonoBehaviour
+{
+    public float rotationSpeed = 100f; // Tốc độ quay của bẫy
+    public float moveSpeed = 5f; // Tốc độ di chuyển của bẫy
+    public Vector3 moveDirection = Vector3.forward; // Hướng di chuyển của bẫy
+
+    void Update()
+    {
+        // Quay bẫy xung quanh trục 
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+        // Di chuyển bẫy theo hướng và tốc độ đã chỉ định
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
